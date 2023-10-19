7ab8230b2108c1969ba3284c914047182295cc1f
ciraganenicole <ciraganenicole@gmail.com>
2023-10-19 12:44:36 -0700

    Add onboarding page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Signup from './pages/auth/signup'
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Onboarding from './pages/onboarding';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <Provider store={store}>
-      <div className="App">
-        <Signup />
-      </div>
+        <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </Provider>
   )
 }
