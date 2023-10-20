1331e8bc4a53a4091990380561ce7219eda55432
jefftrojan <j.dauda@alustudent.com>
2023-10-20 10:30:26 +0200

    Reset password

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,24 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import LoginPage from './components/login/LoginPage.tsx'
+import ResetPassword from './components/reset-password/ResetPassword.tsx'
+import ConfirmPassword from './components/reset-password/ConfirmPassword.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState('reset'); // 'reset' or 'confirm'
+
+  const switchToConfirmPage = () => {
+    setCurrentPage('confirm');
+  };
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
-      <LoginPage />
-    </>
-  )
+    <div className="App">
+      {currentPage === 'reset' && (
+        <ResetPassword switchToConfirmPage={switchToConfirmPage} />
+      )}
+      {currentPage === 'confirm' && <ConfirmPassword />}
+    </div>
+  );
 }
 
 export default App
